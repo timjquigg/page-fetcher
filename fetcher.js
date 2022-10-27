c7fcf32405c5f11463d43156729f4b746e6dcb3a
Tim Quigg <timjquigg@gmail.com>
2022-10-27 10:33:22 -0600

    Completed functionality to check for file path and ask user for new path if required, or overwrite.

--- fetcher.js
+++ fetcher.js
@@ -1,5 +1,8 @@
+fs = require('fs');
 const request = require('request');
-const fs = require('fs');
+const { checkFile } = require('./userInput');
+
+let data;
 
 const printConfirmation = (file) => {
   fs.stat(file, (err, stats) => {
@@ -7,20 +10,31 @@ const printConfirmation = (file) => {
   });
 };
 
-const writePage = (file, data, printConfirmation) => {
+
+const fileComplete = (inputFile) => {
+  file = inputFile;
+  writePage(file, data);
+};
+
+const pageComplete = (inputData) => {
+  data = inputData;
+  checkFile(file, fileComplete);
+
+};
+
+const writePage = (file, data) => {
+  
   fs.writeFile(file, data, (err) => {
     if (err) throw err;
     printConfirmation(file);
   });
 };
 
-const fetchPage = (address, file, writePage) => {
+const fetchPage = (address) => {
   
   request(address, (error, response, body) => {
-    // console.log('error:', error); // Print the error if one occurred
-    // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    // console.log('body:', body); // Print the HTML for the Google homepage.
-    writePage(file, body, printConfirmation);
+    console.log(`statusCode: ${response.statusCode}: ${response.statusMessage}`);
+    pageComplete(body);
   });
   
 };
@@ -28,7 +42,9 @@ const fetchPage = (address, file, writePage) => {
 
 const input = process.argv.slice(2);
 const address = input[0];
-const file = input[1];
+let file = input[1];
 
-fetchPage(address, file, writePage);
+fetchPage(address, pageComplete);
+
+module.exports = { fileComplete };
 
--- userInput.js
+++ userInput.js
@@ -1,3 +1,5 @@
+const fileComplete = require('./fetcher');
+const fs = require('fs');
 const readline = require('readline');
 
 const rl = readline.createInterface({
@@ -5,19 +7,32 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
-const complete = () => {
-  console.log('We done');
-};
-
-const questions = () => {
-  rl.question('Are we all good?\n', (answer) => {
-    if (answer === 'y') {
-      complete();
+const checkFile = (file, fileComplete) => {
+  fs.stat(file, (err, stats) => {
+    if (err) {
+      rl.question('Invalid file path, please enter a new path.\n', (answer) => {
+        checkFile(answer, fileComplete);
+      });
+    } else if (stats) {
+      rl.question('File already exists. Would you like to overwite? (y/n)\n', (answer) => {
+        if (answer.toLowerCase() === 'y') {
+          console.log('Overwriting...');
+          rl.close();
+          fileComplete(file);
+        }
+        if (answer.toLowerCase() === 'n') {
+          rl.question('Please enter new fie path.\n', (answer) => {
+            checkFile(answer, fileComplete);
+          });
+        }
+      });
+    } else {
+      fileComplete(file);
       rl.close();
-      return;
     }
-    questions();
   });
 };
 
-questions();
+module.exports = {
+  checkFile
+};
